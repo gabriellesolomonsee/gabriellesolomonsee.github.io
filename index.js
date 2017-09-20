e15a182d8d107eb386dec402c0d304400dc4e027
gabriellesolomonsee <gabrielle@solomonsee.com>
2017-09-20 15:45:00 +0800

    JS 20Sep1544

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 alert("Welcome to JS class.")
+
+var student="Gabrielle";
+
+
+
+var student_1="Gabrielle";
+var student_2="Wei Bin"
+
+var console_msg="Hi! Let's be friend.."
+
+console.log(console_msg+student_1+student_2);

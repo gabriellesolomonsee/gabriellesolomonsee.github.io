234009569e38177ba6de24cca9215cdbc46c0b36
gabriellesolomonsee <gabrielle@solomonsee.com>
2017-09-21 12:26:19 +0800

    My Profile 21Sep1226

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ var student_2="Wei Bin"
 var console_msg="Hi! Let's be friend.."
 
 console.log(console_msg+student_1+student_2);
+
+function greet(nameOfPerson){var greeting_message="Welcome, Mr" + nameOfPersonconsole.loh(greeting_message)}
+
+Greet("Bala")
+
+<script src="index.html">
+    
+function friendName(name1,name2){
+    var ans=name1+" "+name1+" | "+name2+" "+name2;
+    return ans;
+}
+
+var statement=friendName("Gabrielle", "Wei Bin");
+
+console.log(statement)
+

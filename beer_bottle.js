574592e8cb32fd611bce6c3a18a22c6f0197b661
gabriellesolomonsee <gabrielle@solomonsee.com>
2017-09-22 06:06:28 +0800

    Assignment 2

---
+++ beer_bottle.js
@@ -0,0 +1,6 @@
+for(var n=99;n>1;n=n-1) {
+    var nextNumber=n-1
+    console.log(n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + nextNumber + " bottles of beer on the wall.");
+    }
+
+    console.log(n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + nextNumber + " bottles of beer on the wall.");

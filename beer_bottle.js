9dbf24199de7a8746860d655baf2708e58681b1d
gabriellesolomonsee <gabrielle@solomonsee.com>
2017-09-22 10:20:18 +0800

    Updated form submission url

--- beer_bottle.js
+++ beer_bottle.js
@@ -2,5 +2,5 @@ for(var n=99;n>1;n=n-1) {
     var nextNumber=n-1
     console.log(n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + nextNumber + " bottles of beer on the wall.");
     }
-
-    console.log(n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + nextNumber + " bottles of beer on the wall.");
+    var lastNumber=n-1
+    console.log(n + " bottle of beer on the wall, " + n + " bottle of beer. Take one down and pass it around, " + lastNumber + " bottle of beer on the wall.");
